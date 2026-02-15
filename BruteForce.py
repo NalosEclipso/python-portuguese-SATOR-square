20ef7126d4dbd19831bd95f951febf080a6b359b
NalosEclipso <140351423+NalosEclipso@users.noreply.github.com>
2026-02-15 03:15:52 -0300

    Create BruteForce.py

---
+++ BruteForce.py
@@ -0,0 +1,14 @@
+from verification import SATOR
+
+with open("Dic/h.txt", "r", encoding="utf-8") as f:
+    h_words = [line.strip() for line in f]
+
+with open("Dic/p.txt", "r", encoding="utf-8") as f:
+    p_words = [line.strip() for line in f]
+
+solution = []
+for h1 in h_words:
+    for h2 in h_words:
+      for p in p_words:
+        if SATOR(h1, h2, p):
+          solution.append([h1,h2,p])
