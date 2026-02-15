a6b6ae2eb62591e68e760232c5ab4675b028e7e0
NalosEclipso <140351423+NalosEclipso@users.noreply.github.com>
2026-02-15 02:08:23 -0300

    Create verifcation.py

---
+++ verifcation.py
@@ -0,0 +1,20 @@
+def SATOR(h1, h2, p):
+    """Verify if those words can be a sator square"""
+    sq = [h1, h2, p, h2[::-1], h1[::-1]]
+    n=0
+    L=''
+    U=''
+    for word in sq:
+        L=L+word
+    while n<5:
+        for word in sq:
+            U=U+word[n]
+        n+=1
+    return L == U == L[::-1] == U[::-1]
+
+    
+#Examples:
+
+print(SATOR('SATOR', 'AREPO', 'TENET')) #Should be True
+print(SATOR('SATOR', 'BUNDA', 'TENET')) #Should be False
+print(SATOR('ENALD', 'INHOS', 'LOUCO')) #Should be False
